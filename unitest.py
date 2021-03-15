43c04ae8730d96b4183ee6a25cf767dd177690f4
Jordan-Fakers <Jordan.j.talvin@gmail.com>
2021-03-15 12:56:42 +0100

    initialisation des tests

---
+++ unitest.py
@@ -0,0 +1,10 @@
+import unittest 
+import os
+
+class Testing_file(unittest.TestCase):
+    def test_check_file_exist(self)
+    directory_path = './main.py'
+    self assertTrue(os.path.exists(directory_path))
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
